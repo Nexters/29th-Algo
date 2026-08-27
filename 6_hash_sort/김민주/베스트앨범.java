6c3847be88bb55b87151133213468d7291c0e7f0
mjk25 <winsomjk@gmail.com>
2026-08-27 15:19:37 +0900

    [김민주] 베스트앨범 수정

--- 6_hash_sort/김민주/베스트앨범.java
+++ 6_hash_sort/김민주/베스트앨범.java
@@ -2,38 +2,62 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        int n = genres.length();
-        HashMap <String, int[]> map = new HashMap<>();
+        ArrayList <Integer> answer = new ArrayList<>();
+        int n = genres.length;
+        // 장르, 재생 총합
+        HashMap <String, Integer> gen = new HashMap<>();
+        // 장르 - idx, song재생 횟수
+        HashMap <String, HashMap <Integer, Integer>> map = new HashMap<>();
+        
         
         for (int i=0; i<n; i++){
-            if (map.containsKey(genres[i])){
-                
-                int[] plist = Arrays.copyOf(map.get(genres[i]), 3); 
-                plist[2] = plays[i];
-
-                Arrays.sort(plist, Collections.reverseOrder());
-                map.put(genres[i], {plist[0], plist[1], 0});
+            HashMap <Integer, Integer> music = new HashMap<>();
+            if (gen.containsKey(genres[i])){
+                music.put(i, plays[i]);
+                gen.put(genres[i], gen.get(genres[i]) + plays[i]);
+                map.get(genres[i]).put(i, plays[i]);
+                // int[] plist = Arrays.copyOf(map.get(genres[i]), 3); 
+                // Arrays.sort(plist, Collections.reverseOrder());
+                // map.put(genres[i], {i, plist[i]});
             }else{
-                map.put(genres[i], {plays[i], 0, 0});
+                music.put(i, plays[i]);
+                gen.put(genres[i], plays[i]);
+                map.put(genres[i], music);
+            }
+        }
+        int mlen = gen.size();
+        // 리스트로 변경 : 장르, 재생 총합이니까 장르를 리스트로, 총합 기준 정렬.
+        List<String> keySet = new ArrayList<>(gen.keySet());
+        // 정렬
+        keySet.sort((g1, g2) -> gen.get(g2) - gen.get(g1));
+        
+        for (int i=0; i<mlen; i++){
+            String nowG = keySet.get(i);
+            HashMap <Integer, Integer> nowGenre = map.get(nowG);
+            // 리스트로 변경 :  idx를 리스트로, 재생횟수 기준 정렬. 
+            List <Integer> idxSong = new ArrayList<>(nowGenre.keySet());
+            // 정렬
+            idxSong.sort((s1, s2) -> {
+                if (nowGenre.get(s2).equals(nowGenre.get(s1))){
+                    return s1 - s2;
+                }else{
+                    return nowGenre.get(s2) - nowGenre.get(s1);
+                }
+            });
+                    
+                        
+            // 가장 큰수, 다음 큰수 i 넣기
+            answer.add(idxSong.get(0));
+            if (idxSong.size() > 1 ){
+                answer.add(idxSong.get(1));
             }
         }
         
-        int mlen = map.size();
+        // int[] ans = new int[answer.size()];
+        // for (int i=0; i<answer.size(); i++ ){
+        //     ans[i] = answer.get(i);
+        // }
+        return answer.stream().mapToInt(x -> x).toArray();
         
-        for (int i=0; i<mlen; i++){
-            
-        }
-        
-        return answer;
     }
 }
-
-
-
-
-
-
-
-
-
